9377151bf3974d8ccec3cdd5dd10caa1f48f1455
Renée Hendricksen <git@reneedv.com>
2019-07-10 16:08:32 -0700

    gets and verifies the most recent 1100 builds for rails

--- get_rails_builds.rb
+++ get_rails_builds.rb
@@ -7,15 +7,25 @@ headers = {
   "Authorization"=> "token #{ENV['TRAVIS_TOKEN']}"
 }
 
-response = HTTParty.get('https://api.travis-ci.org/repo/891/builds?limit=100', {
-  headers: headers
-})
-#binding.pry
+response = HTTParty.get('https://api.travis-ci.org/repo/891/builds?limit=100', headers: headers)
+
+builds = {builds: response["builds"]}
+
 puts response["builds"].count
-puts response["@pagination"]["next"]["@href"]
-
-builds = {builds: response["builds"]}.to_json
-
-open('rails_builds.json', 'a') do |f|
-  f << builds
+10.times do
+  puts response["@pagination"]["next"]["@href"]
+  next_page = "https://api.travis-ci.org#{response["@pagination"]["next"]["@href"]}"
+  response = HTTParty.get(next_page, headers: headers)
+  puts response["builds"].count
+  builds[:builds] += response["builds"]
 end
+
+puts builds[:builds].count
+
+open('rails_builds.json', 'w') do |f|
+  f << builds.to_json
+end
+
+builds_from_file = JSON.parse(open('rails_builds.json').read)
+# binding.pry
+puts builds_from_file["builds"].count
